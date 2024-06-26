a21ccbc9232d3431c2dfe8f221b1f2380b50e8b5
Diego Iglesias <diegoiglesiasrojo@gmail.com>
2024-06-26 01:49:33 -0300

    v0.5.0 | Created fetch service from battleList.

--- src/app/battlelist/page.jsx
+++ src/app/battlelist/page.jsx
@@ -1,7 +1,59 @@
+"use client";
+
+import styles from "./battleList.module.css";
+import { useState, useEffect } from "react";
+import { Button, Box, CircularProgress } from "@mui/material";
+import { readBattleList } from "@/services/battleList.services";
+import Toast from "@/components/toast/Toast";
+
 const BattleList = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isToastVisible, setIsToastVisible] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [message, setMessage] = useState("Unexpected error. Please try later");
+  const [listOfBattles, setListOfBattles] = useState([]);
+
+  const handleClose = () => {
+    setIsToastVisible(false);
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      const response = await readBattleList();
+      if (response.success) {
+        setListOfBattles(response.response.response);
+        setIsError(false);
+      } else {
+        setMessage(response.error);
+        setIsError(true);
+        setIsToastVisible(true);
+      }
+    };
+    fetchData();
+    setIsLoading(false);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main className={styles.main}>
+        <Box sx={{ display: "flex" }}>
+          <CircularProgress color='success' />
+        </Box>
+      </main>
+    );
+  }
+
+  console.log(listOfBattles);
   return (
-    <main>
-      <p>Battle List</p>
+    <main className={styles.main}>
+      <h1>Battle List</h1>
+      <Toast
+        isToastVisible={isToastVisible}
+        handleClose={handleClose}
+        isErrorToast={isError}
+        message={message}
+      />
     </main>
   );
 };
--- src/services/battleList.services.js
+++ src/services/battleList.services.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const createNewPokemon = async (newPokemon) => {
+export const readBattleList = async () => {
   return await axios
-    .post("http://localhost:4000/api/pokemon", { ...newPokemon })
+    .get("http://localhost:4000/api/battle")
     .then((response) => {
       const res = {
         success: true,
@@ -11,10 +11,10 @@ export const createNewPokemon = async (newPokemon) => {
       return res;
     })
     .catch((e) => {
-      const messageError = e.response.data.error
-        ? e.response.data.error
-        : e.response.data.validationError
-        ? e.response.data.validationError
+      const messageError = e?.response?.data?.error
+        ? e.response?.data?.error
+        : e.response?.data?.validationError
+        ? e.response?.data?.validationError
         : "Unexpected error. Please try later";
 
       const response = {
